72c911f713e36d20e67404289adaa27ef27cb87d
iannathan-k <ianmtv@gmail.com>
2026-02-09 16:50:30 -0800

    finished for now, need to ask a q

--- main.cpp
+++ main.cpp
@@ -11,10 +11,9 @@ string* reverseArray(string* ptr);
 
 void displayArray(string* ptr);
 
-
 int main() {
     string* array = new string[SIZE];
-    *(array + 0) = "Alice";
+    *(array + 0) = "Alice"; // init with some familiar names
     *(array + 1) = "Bob";
     *(array + 2) = "Charlie";
     *(array + 3) = "David";
@@ -23,21 +22,33 @@ int main() {
     string* new_array = reverseArray(array);
 
     cout << "Original array: ";
+    displayArray(array);
+    cout << "Reversed array: ";
+    displayArray(new_array);
+
+    delete[] array;
+    delete[] new_array;
 
     return 0;
 }
 
-// are we returning a pointer to a new array or the old one modified?
+// reverseArray() reverses the array and returns a pointer to the modified array
+// arguments: string* ptr, pointing to the old array
+// returns: string* new_array, pointing to the modified array
 string* reverseArray(string* ptr) {
     string* new_array = new string[SIZE];
 
     for (int i = 0; i < SIZE; i++) {
-        *(new_array + i) = *(ptr + SIZE - i);
+        // ptr + SIZE - i - 1, accesses the i-th element from the end
+        *(new_array + i) = *(ptr + SIZE - i - 1);
     }
 
     return new_array;
 }
 
+// displayArray() prints out the array in a single line
+// arguments: string* ptr, pointing to the old array
+// returns: none
 void displayArray(string* ptr) {
     for (int i = 0; i < SIZE; i++) {
         cout << *(ptr + i) << " ";
